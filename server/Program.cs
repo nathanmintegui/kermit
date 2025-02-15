e8bb80c31f6679a2a071af869e104022c233e4e4
nathanmintegui <nathanberger.rs@hotmail.com>
2025-02-15 11:43:49 -0300

    feat: setup db connection factory

--- server/Program.cs
+++ server/Program.cs
@@ -1,8 +1,13 @@
+using Kermit.Database;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<IDbConnectionFactory>(_ =>
+    new NpgsqlDbConnectionFactory(builder.Configuration["DbConnectionString"]!));
 
 WebApplication app = builder.Build();
 
