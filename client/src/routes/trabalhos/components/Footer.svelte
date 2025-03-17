055ac4df8fb84296916457e5f658d76abf9f85ec
nathanmintegui <nathanberger.rs@hotmail.com>
2025-03-17 15:05:15 -0300

    styles: add css background pattern on /trabalhos route

---
+++ client/src/routes/trabalhos/components/Footer.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	const secoes = ['Trabalhos', 'Calendário'];
+
+</script>
+
+<div class="w-screen border-t absolute bottom-0 flex gap-5">
+	<div class="flex gap-5 p-1">
+		{#each secoes as secao }
+			<p class="hover:cursor-pointer hover:font-bold pr-3">{secao}</p>
+		{/each}
+	</div>
+</div>
